147860ac1667b3a835ac365a1b719d8e9f63bbe7
hyejin-0 <hj111800@naver.com>
2023-11-19 22:17:20 +0900

    3

--- main.c
+++ main.c
@@ -3,15 +3,20 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+void swap(int *x, int *y)
+{
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
 int main(void)
 {
-	int i = 4;
-	char c = 'A';
-	
-	int *pi = &i;
-	char *pc = &c;
-	
-	printf("%i, %i, %c, %c\n", i, *pi, c, *pc);
+	int a = 3;
+	int b = 5;
+	swap(&a, &b);
+	printf("a:%i, b:%i\n", a, b);
 
 	return 0;
 }
